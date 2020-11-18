21f2910cf0643373da1a04aea0be2404f2ddf9dc
Khalid <er.kmk123@gmail.com>
2020-11-18 10:53:24 +0530

    api Schema

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -14,4 +16,10 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('payments/', include('payments.urls')),
+    path('docs/', include_docs_urls(title='managerapp')),
+    path('schema', get_schema_view(
+        title="ManagerApp",
+        description="API for the ManagerApp",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
